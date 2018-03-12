2875ea2d2accc23d4cc7e05f1ea8f33622548537
Chris Keller <christopherlawrencekeller@gmail.com>
2018-03-12 09:41:35 -0700

    reconfigures script

--- init.py
+++ init.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
+import pickle
 import time
 import datetime
 import string
@@ -21,18 +22,41 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-class BeginCommand(object):
+class CannabisLicense(object):
+
+    def __init__(self, date_added, details_link, permit_id, permit_type, legal_business_name, expires_on, status, status_date, license_address, first_name, last_name, biz_name, biz_name_extra, biz_country, biz_phone, biz_email, org_structure, app_info_list, notes, *args, **kwargs):
+        self.date_added = date_added
+        self.details_link = details_link
+        self.permit_id = permit_id
+        self.permit_type = permit_type
+        self.legal_business_name = legal_business_name
+        self.expires_on = expires_on
+        self.status = status
+        self.status_date = status_date
+        self.license_address = license_address
+        self.first_name = first_name
+        self.last_name = last_name
+        self.biz_name = biz_name
+        self.biz_name_extra = biz_name_extra
+        self.biz_country = biz_country
+        self.biz_phone = biz_phone
+        self.biz_email = biz_email
+        self.org_structure = org_structure
+        self.app_info_list = app_info_list
+        self.notes = notes
+
+
+class ConstructCannabisLicenses(object):
 
     help = "Begin request to pull cannabis licenses"
 
     def __init__(self):
         self.url_prefix = 'https://aca5.accela.com'
         self.url = 'https://aca5.accela.com/bcc/Cap/CapHome.aspx?module=Licenses&ShowMyPermitList=N'
-        self.driver = webdriver.Chrome('/Users/user/_programming/_lat/_projects/2018_01_18_state_cannabis_licenses/chromedriver')
-        self.driver.set_window_size(1200, 960)
         self.start_date = None
         self.end_date = None
-        self.list_of_permits = []
+        self.list_of_licenses = []
+        self.text_filename = "list_of_permits.txt"
         self.csv_filename = "cannabis_licenses.csv"
         self.csv_headers = [
             'date_added',
@@ -56,11 +80,32 @@ class BeginCommand(object):
             'notes',
         ]
 
-    def clear_loading_time(self, seconds, element):
-        wait = WebDriverWait(self.driver, seconds)
+    def handle(self):
+        # logger.info("Beginning to scrape state licenses" % (len(self.list_of_licenses)))
+        # self.scrape()
+        # logger.info("Writing %s state licenses to a text file" % (len(self.list_of_licenses)))
+        # with open(self.text_filename, 'wb') as text_file:
+        #     pickle.dump(self.list_of_licenses, text_file)
+        with open (self.text_filename, 'rb') as pickle_file:
+            self.list_of_licenses = pickle.load(pickle_file)
+        logger.info("Found %s state licenses in the text file" % (len(self.list_of_licenses)))
+        with open(self.csv_filename, "wb") as csv_file:
+            logger.info("Creating %s" % (self.csv_filename))
+            csv_output = csvkit.writer(csv_file, delimiter=",", encoding="utf-8")
+            csv_output.writerow(self.csv_headers)
+            for i in self.list_of_licenses[1124:]:
+                obj = self.structure_data(i)
+                csv_output.writerow(obj)
+        logger.info("%s has been written" % (self.csv_filename))
+
+    def clear_loading_time(self, driver, seconds, element):
+        wait = WebDriverWait(driver, seconds)
         wait.until(lambda driver: driver.find_element_by_id(element).is_displayed() == False)
 
     def process_table_data(self ,soup):
+        '''
+        simplified manner of processing table data
+        '''
         try:
             table = soup.find('table', class_='ACA_GridView ACA_Grid_Caption')
             rows = table.find_all('tr', class_=['ACA_TabRow_Odd ACA_TabRow_Odd_FontSize', 'ACA_TabRow_Even ACA_TabRow_Even_FontSize'])
@@ -74,147 +119,193 @@ class BeginCommand(object):
                 data_dict['expires_on'] = tds[4].get_text(strip=True)
                 data_dict['status'] = tds[5].get_text(strip=True)
                 data_dict['status_date'] = tds[6].get_text(strip=True)
-                self.list_of_permits.append(data_dict)
+                data_dict['date_added'] = datetime.datetime.now()
+                data_dict['notes'] = []
+                self.list_of_licenses.append(data_dict)
         except Exception, exception:
             error_output = "%s" % (exception)
             return False
 
     def scrape(self):
         '''
-        scrape all of the licenses for a given type
+        for each page of licenses, pull in the data
         '''
-        self.driver.get(self.url);
-        self.driver.find_element_by_id('ctl00_PlaceHolderMain_btnNewSearch').click()
-        self.clear_loading_time(30, 'divGlobalLoading')
-        s = BeautifulSoup(self.driver.page_source, 'html5lib')
+        driver = webdriver.Chrome('/Users/user/_programming/_lat/_code/calif_cannabis_licenses/chromedriver')
+        driver.set_window_size(1200, 960)
+        driver.get(self.url);
+        driver.find_element_by_id('ctl00_PlaceHolderMain_btnNewSearch').click()
+        self.clear_loading_time(driver, 30, 'divGlobalLoading')
+        s = BeautifulSoup(driver.page_source, 'html.parser')
         self.process_table_data(s)
         proceed = True
         while proceed == True:
             try:
-                next_page_elem = self.driver.find_element_by_xpath("//a[text()='Next >']")
+                next_page_elem = driver.find_element_by_xpath("//a[text()='Next >']")
                 logger.info(next_page_elem)
                 next_page_elem.click()
-                self.clear_loading_time(30, 'divGlobalLoading')
-                s = BeautifulSoup(self.driver.page_source, 'html5lib')
+                self.clear_loading_time(driver, 30, 'divGlobalLoading')
+                s = BeautifulSoup(driver.page_source, 'html.parser')
                 self.process_table_data(s)
             except NoSuchElementException, exception:
                 error_output = "%s" % (exception)
                 logger.info(error_output)
                 proceed = False
-        logger.info(len(self.list_of_permits))
-        with open(self.csv_filename, "wb") as csv_file:
-            csv_output = csvkit.writer(csv_file, delimiter=",", encoding="utf-8")
-            csv_output.writerow(self.csv_headers)
-            print self.list_of_permits
-            for permit in self.list_of_permits:
-                permit['notes'] = None
-                self.driver.get(permit['details_link']);
-                try:
-                    self.driver.find_element_by_id('lnkMoreDetail').click()
-                    self.driver.find_element_by_id('lnkRc').click()
-                    self.driver.find_element_by_id('lnkASITableList').click()
-                except NoSuchElementException, exception:
-                    permit['notes'] = exception
-                    pass
-                time.sleep(15)
-                s = BeautifulSoup(self.driver.page_source, 'html5lib')
-                try:
-                    target_address = s.find('div', class_='div_parent_detail').find('tbody').find_all('td')
-                    permit['license_address'] = target_address[0].get_text(separator=u' ', strip=True)
-                except NoSuchElementException, exception:
-                    permit['notes'] = exception
-                    pass
-                try:
-                    r = s.find_all('td', class_='MoreDetail_BlockContent')
-                except NoSuchElementException, exception:
-                    r = None
-                    permit['notes'] = exception
-                    pass
-                if r:
-                    # business information
-                    try:
-                        permit['first_name'] = r[0].find('span', class_='contactinfo_firstname').get_text(strip=True)
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['first_name'] = None
-                    try:
-                        permit['last_name'] = r[0].find('span', class_='contactinfo_lastname').get_text(strip=True)
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['last_name'] = None
-                    try:
-                        permit['biz_name'] = r[0].find('span', class_='contactinfo_businessname').get_text(strip=True)
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['biz_name'] = None
-                    try:
-                        permit['biz_name_extra'] = r[0].find('span', class_='contactinfo_businessname2').get_text(strip=True)
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['biz_name_extra'] = None
-                    try:
-                        permit['biz_country'] = r[0].find('span', class_='contactinfo_country').get_text(strip=True)
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['biz_country'] = None
-                    try:
-                        permit['biz_phone'] = r[0].find('div', class_='ACA_PhoneNumberLTR').get_text(strip=True)
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['biz_phone'] = None
-                    try:
-                        permit['biz_email'] = r[0].find('span', class_='contactinfo_email').get_text(strip=True).replace('E-mail:', '')
-                    except Exception, exception:
-                        logger.info(exception)
-                        permit['biz_email'] = None
-                    # business org structure
-                    try:
-                        org_structure_data = r[0].find('div', class_='ACA_TabRow').find('ul').find('p')
-                        org_structure_data = org_structure_data.get_text(strip=True).split(':')
-                        permit['org_structure'] = org_structure_data[1].strip()
-                    except NoSuchElementException, exception:
-                        logger.info(exception)
-                        raise
-                    # application information lists
-                    try:
-                        span_list = []
-                        spans = r[-1].find_all('span', class_='ACA_SmLabel ACA_SmLabel_FontSize')
-                        for s in spans:
-                            proper_text = s.get_text(strip=True)
-                            span_list.append(proper_text)
-                        permit['app_info_list'] = " ".join(span_list)
-                    except Exception, exception:
-                        logger.info(exception)
-                        raise
-                else:
-                    permit['org_structure'] = None
-                    permit['app_info_list'] = None
-                permit['date_added'] = datetime.datetime.now()
-                logger.info(permit)
-                csv_row_data = [
-                    permit['date_added'],
-                    permit['details_link'],
-                    permit['permit_id'],
-                    permit['permit_type'],
-                    permit['legal_business_name'],
-                    permit['expires_on'],
-                    permit['status'],
-                    permit['status_date'],
-                    permit['license_address'],
-                    permit['first_name'],
-                    permit['last_name'],
-                    permit['biz_name'],
-                    permit['biz_name_extra'],
-                    permit['biz_country'],
-                    permit['biz_phone'],
-                    permit['biz_email'],
-                    permit['org_structure'],
-                    permit['app_info_list'],
-                    permit['notes'],
-                ]
-                csv_output.writerow(csv_row_data)
-        self.driver.quit()
+        driver.quit()
+
+    def structure_data(self, i):
+        time.sleep(5)
+        driver = webdriver.Chrome('/Users/user/_programming/_lat/_code/calif_cannabis_licenses/chromedriver')
+        driver.set_window_size(1200, 960)
+        driver.get(i['details_link']);
+        try:
+            driver.find_element_by_id('lnkMoreDetail').click()
+            driver.find_element_by_id('lnkRc').click()
+            driver.find_element_by_id('lnkASITableList').click()
+        except NoSuchElementException, exception:
+            i['license_address'] = None
+            i['first_name'] = None
+            i['last_name'] = None
+            i['biz_name'] = None
+            i['biz_name_extra'] = None
+            i['biz_country'] = None
+            i['biz_phone'] = None
+            i['biz_email'] = None
+            i['org_structure'] = None
+            i['app_info_list'] = None
+            i['notes'].append("additional details not available")
+            logger.error(exception)
+            driver.quit()
+            logger.info(i)
+            return [
+                i['date_added'],
+                i['details_link'],
+                i['permit_id'],
+                i['permit_type'],
+                i['legal_business_name'],
+                i['expires_on'],
+                i['status'],
+                i['status_date'],
+                i['license_address'],
+                i['first_name'],
+                i['last_name'],
+                i['biz_name'],
+                i['biz_name_extra'],
+                i['biz_country'],
+                i['biz_phone'],
+                i['biz_email'],
+                i['org_structure'],
+                i['app_info_list'],
+                i['notes'],
+            ]
+        s = BeautifulSoup(driver.page_source, 'html.parser')
+        try:
+            target_address = s.find('div', class_='div_parent_detail').find('tbody').find_all('td')
+            i['license_address'] = target_address[0].get_text(separator=u' ', strip=True)
+        except NoSuchElementException, exception:
+            i['license_address'] = None
+            i['notes'].append("license address not found")
+        try:
+            r = s.find_all('td', class_='MoreDetail_BlockContent')
+        except NoSuchElementException, exception:
+            r = None
+            i['notes'].append("more detail not found")
+            pass
+        if r:
+            try:
+                i['first_name'] = r[0].find('span', class_='contactinfo_firstname').get_text(strip=True)
+            except Exception, exception:
+                i['first_name'] = None
+                i['notes'].append("first name not found")
+                logger.error(exception)
+            try:
+                i['last_name'] = r[0].find('span', class_='contactinfo_lastname').get_text(strip=True)
+            except Exception, exception:
+                i['last_name'] = None
+                i['notes'].append("last name not found")
+                logger.error(exception)
+            try:
+                i['biz_name'] = r[0].find('span', class_='contactinfo_businessname').get_text(strip=True)
+            except Exception, exception:
+                i['biz_name'] = None
+                i['notes'].append("business name not found")
+                logger.error(exception)
+            try:
+                i['biz_name_extra'] = r[0].find('span', class_='contactinfo_businessname2').get_text(strip=True)
+            except Exception, exception:
+                i['biz_name_extra'] = None
+                logger.error(exception)
+            try:
+                i['biz_country'] = r[0].find('span', class_='contactinfo_country').get_text(strip=True)
+            except Exception, exception:
+                i['biz_country'] = None
+                i['notes'].append("business country not found")
+                logger.error(exception)
+            try:
+                i['biz_phone'] = r[0].find('div', class_='ACA_PhoneNumberLTR').get_text(strip=True)
+            except Exception, exception:
+                i['biz_phone'] = None
+                i['notes'].append("business phone not found")
+                logger.error(exception)
+            try:
+                i['biz_email'] = r[0].find('span', class_='contactinfo_email').get_text(strip=True).replace('E-mail:', '')
+            except Exception, exception:
+                i['biz_email'] = None
+                i['notes'].append("business email not found")
+                logger.error(exception)
+            try:
+                org_structure_data = r[0].find('div', class_='ACA_TabRow').find('ul').find('p')
+                org_structure_data = org_structure_data.get_text(strip=True).split(':')
+                i['org_structure'] = org_structure_data[1].strip()
+            except NoSuchElementException, exception:
+                i['org_structure'] = None
+                i['notes'].append("org structure not found")
+                logger.error(exception)
+            try:
+                span_list = []
+                spans = r[-1].find_all('span', class_='ACA_SmLabel ACA_SmLabel_FontSize')
+                for s in spans:
+                    proper_text = s.get_text(strip=True)
+                    span_list.append(proper_text)
+                i['app_info_list'] = " ".join(span_list)
+            except Exception, exception:
+                i['app_info_list'] = None
+                i['notes'].append("application info not found")
+                logger.error(exception)
+        else:
+            i['first_name'] = None
+            i['last_name'] = None
+            i['biz_name'] = None
+            i['biz_name_extra'] = None
+            i['biz_country'] = None
+            i['biz_phone'] = None
+            i['biz_email'] = None
+            i['org_structure'] = None
+            i['app_info_list'] = None
+        i['notes'] = "|".join(i['notes'])
+        driver.quit()
+        logger.info(i)
+        return [
+            i['date_added'],
+            i['details_link'],
+            i['permit_id'],
+            i['permit_type'],
+            i['legal_business_name'],
+            i['expires_on'],
+            i['status'],
+            i['status_date'],
+            i['license_address'],
+            i['first_name'],
+            i['last_name'],
+            i['biz_name'],
+            i['biz_name_extra'],
+            i['biz_country'],
+            i['biz_phone'],
+            i['biz_email'],
+            i['org_structure'],
+            i['app_info_list'],
+            i['notes'],
+        ]
 
 if __name__ == '__main__':
-    task_run = BeginCommand()
-    task_run.scrape()
+    task_run = ConstructCannabisLicenses()
+    task_run.handle()
